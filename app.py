ce7394d4bc690535bd4341c48c403e333722df07
Venkatesh Tadinada <ven@solivarlabs.com>
2019-02-20 16:19:59 -0800

    create Diagnosis Image Function

--- app.py
+++ app.py
@@ -10,6 +10,33 @@ def suffix(d):
 def custom_strftime(format, t):
     return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
 
+import random
+
+def DiagnoseImage(ImageID,ImageType):
+    DiagJSON = dict()
+    DiagBoxes = dict()
+    chest_diseases = ["Pneumonia","PneumoThorax","Infusion","CardioMegaly","Nodule","Bronchitis"]
+    breast_diseases = ["Calcification","Circumscribed","Spiculated","Ill-defined","Distortion","Asymmetry"]
+    retina_diseases = ["Macular Degeneration","Melanoma - Cancer","Diabetic Retinopathy",
+                       "Glaucoma","Hypertension","Bronchitis"]
+    c = random.sample(range(20, 79), 6)
+
+    if ImageType == "ChestX-Ray":
+        for i in range(len(c)):
+          DiagJSON[chest_diseases[i]] = c[i]/100
+
+    if ImageType == "BreastCancer":
+        for i in range(len(c)):
+          DiagJSON[breast_diseases[i]] = c[i]/100
+
+    if ImageType == "RetinaImage":
+        for i in range(len(c)):
+          DiagJSON[retina_diseases[i]] = c[i]/100
+
+    return DiagJSON, DiagBoxes
+
+
+
 
 @app.route('/')
 def index():
